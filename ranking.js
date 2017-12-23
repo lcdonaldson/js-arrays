b235bba859b4a1dd3430331ea280820b58141c3d
Levi Donaldson <lcdbacanas@gmail.com>
2017-12-22 20:33:18 -0600

    Update ranking.js

--- ranking.js
+++ ranking.js
@@ -1,3 +1,6 @@
+// The input represents an array of scores from players. We want to display those scores
+// as highest scores ranked 1. So in this case scores 1, 5, 2 would equal rank 3, 1, 2.
+  
 function rankings(arr){
 
   let firstMod = arr.slice().sort( (a, b) => b - a);
