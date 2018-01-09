30c584603df6df19cbb7e87aee1c5ee24f94063a
Levi Donaldson <lcdbacanas@gmail.com>
2018-01-09 17:05:29 -0600

    Update array.js

--- array.js
+++ array.js
@@ -123,3 +123,22 @@
 
     console.table(dog);
 
+
+//     counter function
+    var iterable = {
+      [Symbol.iterator]() {
+        return {
+          i: 0,
+          next() {
+            if (this.i < 31) {
+              return { value: this.i++, done: false };
+            }
+            return { value: undefined, done: true };
+          }
+        };
+      }
+    };
+
+    for (var value of iterable) {
+      console.log(value);
+    }
